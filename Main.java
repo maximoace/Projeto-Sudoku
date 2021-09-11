54a3a5247b97e6f8a6f3270514cd320fb6b522ed
Maximoace <maximoarthuro@gmail.com>
2021-09-11 00:48:22 -0300

    Initial commit, added main and game packages

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        /* 
+         * Esta é a classe main, que vai iniciar o jogo,
+         * deve estar o mais limpo possível
+         */
+    }
+}
---
+++ game/GameRules.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * GameRules
+ */
+public class GameRules {
+    /** 
+     * Nessa classe que deve ficar as regras de jogo
+     * Se necessário, pode se criar outra classe só para lidar com os inputs
+     */
+    
+}
